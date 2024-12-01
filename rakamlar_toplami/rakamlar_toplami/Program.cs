294118f7251ae595e4dcec9e1dd857901488cd1b
evrimme <evrimeliacik12@gmail.com>
2024-12-01 13:06:37 +0300

    Add files via upload
    Kullanıcıdan alınan sayının rakamları toplamını c#'ta yazan program.

---
+++ rakamlar_toplami/rakamlar_toplami/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rakamlar_toplami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Kullanıcıdan bir sayı alarak bu sayının basamaklarındaki rakamların toplamını
+              hesaplayan programı yazın.*/
+            Console.Write("Sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int basamak_toplam = 0;
+            int birler;
+            while (sayi != 0)
+            {
+                birler = sayi % 10; //birler basamağı değeri alındı
+                basamak_toplam = basamak_toplam + birler; // birler basamağı değeri toplama eklendi
+                sayi /= 10; //sayı bir basamak sağa kaydırıldı
+            }
+            Console.WriteLine("Basamak Toplamları = " + basamak_toplam);
+            Console.ReadKey();
+        }
+    }
+}
