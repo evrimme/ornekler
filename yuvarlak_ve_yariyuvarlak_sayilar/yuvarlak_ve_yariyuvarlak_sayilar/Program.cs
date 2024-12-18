49e5306a4e10275e7e65408711958446f8873310
Evrim <evrimeliacik12@gmail.com>
2024-12-18 14:55:06 +0300

    Add files via upload
    Girilen [n,m] aralığındaki yuvarlak ve yarı yuvarlak olan sayıları bulan program.

---
+++ yuvarlak_ve_yariyuvarlak_sayilar/yuvarlak_ve_yariyuvarlak_sayilar/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yuvarlak_ve_yariyuvarlak_sayilar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Bir sayı giriniz: ");
+            int m = int.Parse(Console.ReadLine());
+
+            ArrayList yuvarlakSayilar = new ArrayList();
+            ArrayList yariyuvarlakSayilar = new ArrayList();
+            int sayac1 = 0;
+            int sayac2 = 0;
+            for (int i = n; i <= m; i++)
+            {
+                int toplam = 0;
+                int carpim = 1;
+                int sayi = i;
+
+                while (sayi > 0)
+                {
+                    int basamak = sayi % 10;
+                    toplam = toplam + basamak;
+                    carpim = carpim * basamak;
+                    sayi /= 10;
+                }
+                if (toplam == carpim)
+                {
+                    yuvarlakSayilar.Add(i);
+                    sayac1++;
+                }
+                else if (toplam * 2 == carpim)
+                {
+                    yariyuvarlakSayilar.Add(i);
+                    sayac2++;
+                }
+            }
+            if (sayac1 != 0)
+            {
+                Console.WriteLine("\nYuvarlak sayılar");
+                foreach (int eleman in yuvarlakSayilar)
+                {
+                    Console.WriteLine(eleman);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nBu aralıkta yuvarlak sayı yoktur.");
+            }
+            if (sayac2 != 0)
+            {
+                Console.WriteLine("\nYarı yuvarlak sayılar");
+                foreach (int eleman in yariyuvarlakSayilar)
+                {
+                    Console.WriteLine(eleman);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nBu aralıkta yarı yuvarlak sayı yoktur.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ yuvarlak_ve_yariyuvarlak_sayilar/yuvarlak_ve_yariyuvarlak_sayilar/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("yuvarlak_ve_yariyuvarlak_sayilar")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("yuvarlak_ve_yariyuvarlak_sayilar")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("b1a9e545-603f-4ac8-987a-89df69ae1de3")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
